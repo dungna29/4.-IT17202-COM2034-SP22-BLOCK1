cf6b1173669f7aedcfd39ddd737da65ee9c886d0
Dungna89 <dungna29@fpt.edu.vn>
2022-01-11 16:12:38 +0700

    1

---
+++ SQLQuery_NangCao_DungNA29_IT17202_SP22_BL1.sql
@@ -0,0 +1,11 @@
+-- SQLQuery_NangCao_DungNA29_IT17202_SP22_BL1
+-- Các phím tắt cơ bản:
+-- Ctrl + /: Dùng comment code
+-- F5: Dùng để chạy câu lệnh SQL
+
+-- Bài 1 Tạo biến bằng lệnh Declare trong SQL SERVER
+-- 1.1 Để khai báo biến thì các bạn sử dụng từ khóa Declare với cú pháp như sau:
+DECLARE @YEAR AS INT
+
+DECLARE @NAME AS NVARCHAR,
+        @YEAR_OF_BIRTH AS INT,
